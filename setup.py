ecbdc2bade3827b8aaa418fe84a796d0b478561e
Kees Bakker <keestux@gmail.com>
2022-08-11 17:13:35 +0200

    Switch from PyCrypto to PyCryptodome

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name="jenkuc",
-  version="0.1",
+  version="0.1.1",
   description="Simple Jenkins update center implementation",
   url="https://github.com/iamtrump/jenkins-update-center",
   author="Mikhail Mironov",
   author_email="mikhailmironov@mikhailmironov.ru",
   license="MIT",
   packages=["jenkuc"],
-  install_requires=["pycrypto"],
+  install_requires=["pycryptodome"],
   zip_safe=False)
